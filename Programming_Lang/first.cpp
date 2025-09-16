80179edae4f32cd9f9f2480e691313f5b72147ad
nehayadav51 <y.neha895700@gmail.com>
2025-09-16 12:34:57 -0700

    Removed Programming_Lang folder

--- Programming_Lang/first.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    cout<<"100 days of DSA challenge";
-    return 0;
-}
--- Programming_Lang/second.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    cout<<"Neha Yadav"<<endl<<"A-552 SaiSadan"<<endl<<"Sector 19"<<endl;
-    return 0;
-} 
